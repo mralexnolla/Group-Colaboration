c6cc15aa374e6fa176c78949c9480f2c6de3367f
alex <nba4everwanted@gmail.com>
2023-01-26 06:43:45 +0000

    Class colaboration main file

---
+++ index.js
@@ -0,0 +1,6 @@
+let title = prompt("Questions African Mothers would ask you then they want to beat you. Enter ok ");
+let name = prompt("You the child, what is that your name again ");
+let mq1 = prompt(`${name} you dont know how to great? slap`)
+let mas = prompt(`"${mq1}" dont worry i will teach you manners. Another Slap Slap `)
+let mq2 = prompt(`When i beat u dont cry abi. ....Rain of slaps..`)
+let mq3 = prompt(`${mq2} your are still crying you want me to add some`)
