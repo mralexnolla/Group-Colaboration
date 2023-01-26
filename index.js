137a78cafb74ad746372f9acecb4c51e5d0311f9
Adubee <82320818+Adubee@users.noreply.github.com>
2023-01-26 12:42:01 +0000

    changes made by Adubee

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ let mq1 = prompt(`${name} you dont know how to great? slap`)
 let mas = prompt(`"${mq1}" dont worry i will teach you manners. Another Slap Slap `)
 let mq2 = prompt(`When i beat u dont cry abi. ....Rain of slaps..`)
 let mq3 = prompt(`${mq2} your are still crying you want me to add some`)
+// Added by Adubee
+let mq4 = prompt(`${name}, Now say sorry before I add more.`) 
+if (mq4.toLowerCase() == 'sorry'){
+	prompt('Well done, dont do that again')
+} else{
+	prompt('slap, slap')
+}
+// End of Adubee's code
