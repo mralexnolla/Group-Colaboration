8c3273151ffe1c7b735119247c1a618f2692c113
Auly-hub <121553678+Auly-hub@users.noreply.github.com>
2023-02-04 13:47:09 -1200

    aurelie

--- index.js
+++ index.js
@@ -46,3 +46,16 @@ function myResponse(){
 }
 
 // end of Cathy's contribution.
+
+
+
+
+//Aurelia added
+
+let mq8 = prompt(`${name} i'm talking to you, you stare at me, do you challenge me? slap slap`)
+let mq9 = prompt(`${name} i'm talking to you, you don't mind me? slap slap`)
+let mq10 = prompt(`${name} don't speak while i'm speaking to you, slap slap slap`)
+let mq11s = prompt(`${name} ? when grow - up an adults are speaking, you must keep quiet. slap slap`)
+
+//End of Aurelia post
+ 
